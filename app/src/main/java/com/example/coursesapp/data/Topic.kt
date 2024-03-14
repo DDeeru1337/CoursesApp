591f7cce9723fd4d6a74635ed5fdc2fc2667cf54
DDeer <ddeeru1337@gmail.com>
2024-03-14 17:36:50 +0300

    Add Topic

---
+++ app/src/main/java/com/example/coursesapp/data/Topic.kt
@@ -0,0 +1,9 @@
+package com.example.coursesapp.data
+
+import android.graphics.drawable.Drawable
+
+data class Topic(
+    val name : String,
+    val icon : Drawable,
+    val coursesCount : Int
+)
